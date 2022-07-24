ca41ddbe52c7d3a9ef3db293918a2b1e6043e739
lanjewarraj <rajlanjewar98@gmail.com>
2022-07-25 02:12:28 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { takeLast } from 'rxjs';
+
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDO';
+
+  todoList : Todo[] = [];
+  
+  todoFrm = new FormGroup({
+    input: new FormControl('',
+      Validators.required
+    )
+  })
+
+  addTodo(){
+    if(this.todoFrm.value.input != ''){
+
+      const newTodo:Todo = {
+        id: Date.now(),
+        value: this.todoFrm.value.input,
+        isComplete: false,
+        date: Date()
+      }
+
+      this.todoList.push(newTodo);
+
+      console.log(this.todoList);
+
+      this.todoFrm.reset();
+
+    }else{
+      alert('please enter todo value');
+    }
+  }
+
+
+  delTodo(id: number){
+    this.todoList = this.todoList.filter(item => item.id != id);
+    console.log(this.todoList);
+  }
+
+
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+    id: number,
+    value: any,
+    isComplete: boolean,
+    date: string
+}
